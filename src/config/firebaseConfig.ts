efd3a2559a34ce96b247317ea83ecf88a1197fa2
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-04-09 18:53:34 -0500

    Feat: added firestoreRepository file to my repositories folder.

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,7 +1,7 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
 
-import * as serviceAccount from "../drivetime-final-project.json";
+import * as serviceAccount from "../../drivetime-final-project.json";
 
 // Initialize the Firebase app with the service account credentials
 // This step is necessary before you can use any Firebase services
