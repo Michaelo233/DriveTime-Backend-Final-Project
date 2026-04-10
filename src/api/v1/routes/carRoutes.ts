f6eca9bc03d4c4a658fe56f773838b572c516f20
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-04-10 16:15:07 -0500

    Feat: Added Create car, get cars, get car, update car, and delete car routes.

--- src/api/v1/routes/carRoutes.ts
+++ src/api/v1/routes/carRoutes.ts
@@ -3,6 +3,16 @@ import { validateRequest } from "../middleWare/validate";
 import * as carController from "../controllers/carControllers";
 import { carSchemas } from "../validation/carSchemas";
 
-const router = express.Router();
+const carRouter = express.Router();
 
-router.post("/", validateRequest(carSchemas.create), carController.createCarHandler);
+carRouter.post("/cars", validateRequest(carSchemas.create), carController.createCarHandler);
+
+carRouter.get("/cars", carController.getAllCarsHandler);
+
+carRouter.get("/cars/:id", validateRequest(carSchemas.getById), carController.getCarByIdHandler);
+
+carRouter.put("/cars/:id", validateRequest(carSchemas.update), carController.updateCarHandler);
+
+carRouter.delete("/cars/:id", validateRequest(carSchemas.delete), carController.deleteCarHandler);
+
+export default carRouter;
