3405924951e350d325733e08133de1175dc66bfe
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-04-10 16:13:57 -0500

    Feat: Added Create car, get cars, get car, update car, and delete car service function.

--- src/api/v1/services/carServices.ts
+++ src/api/v1/services/carServices.ts
@@ -47,3 +47,112 @@ export const createCar = async (
         );
     }
 };
+
+// to get all cars in a collection 
+export const getAllCars = async (): Promise<Car[]> => {
+    try {
+        // const event = await firestoreRepository.createDocument<Event>(COLLECTION, newEventData);
+        const cars = await firestoreRepository.getAllDocuments<Car>(COLLECTION);
+
+        return cars;
+
+    } catch (error: unknown) {
+        const errorMessage =
+            error instanceof Error ? error.message : "Unknown error";
+        throw new Error(
+            `Failed to retrieve all cars: ${errorMessage}`
+        );
+    }
+};
+
+// to find an existing car by id
+export const getCarById = async (id: string): Promise<Car> => {
+    try {
+        const car = await firestoreRepository.getDocById<Car>(COLLECTION, id);
+
+        if(!car){
+            throw new Error("Car not found");
+        } else {
+            return car;
+        }
+
+
+    } catch (error: unknown) {
+        const errorMessage =
+            error instanceof Error ? error.message : "Unknown error";
+        throw new Error(
+            `Failed to retrieve the car: ${errorMessage}`
+        );
+    }
+};
+
+// updating an existing car - userId or content
+export const updateCar = async (id: string, 
+    carData:{
+        model?: string,
+        brand?: string,
+        year?: number,
+        price?: string,
+        color?: string,
+        status?: string,
+    }): Promise<Car | null> => {
+    try {
+        const updateCarData: Partial<Car> = {};
+        
+        if(carData.model != undefined) {
+            updateCarData.model = carData.model;
+        }
+
+        if(carData.year != undefined) {
+            updateCarData.year = carData.year;
+        }
+
+        if(carData.price != undefined) {
+            updateCarData.price = carData.price;
+        }
+
+        if(carData.color != undefined) {
+            updateCarData.color = carData.color;
+        }
+
+        if(carData.status != undefined) {
+            updateCarData.status = carData.status;
+        }
+
+        if(Object.keys(updateCarData).length === 0){
+            throw new Error("no fields provied to updated");
+        }        
+        
+        updateCarData.updatedAt = new Date().toISOString(); 
+
+        await firestoreRepository.updateDocument(COLLECTION, id, updateCarData);
+
+        const updatedCar = await firestoreRepository.getDocById<Car>(COLLECTION, id);
+
+        if(!updatedCar){
+            throw new Error("Updated car not found");
+        }
+
+        return updatedCar;
+    } catch (error: unknown) {
+        const errorMessage =
+            error instanceof Error ? error.message : "Unknown error";
+        throw new Error(
+            `Failed to update the car: ${errorMessage}`
+        );
+    }
+};
+
+// deleteing an existing car 
+export const deleteCar = async (id: string): Promise <void> => {
+    try {
+        await firestoreRepository.deleteDocument(COLLECTION, id);
+
+    } catch (error: unknown) {
+        const errorMessage =
+            error instanceof Error ? error.message : "Unknown error";
+        throw new Error(
+            `Failed to delete the car: ${errorMessage}`
+        );
+    }
+};
