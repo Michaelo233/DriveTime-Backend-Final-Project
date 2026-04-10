04f14c1e5b758f42fbdad7210cdae2077aac7b1b
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-04-10 16:14:27 -0500

    Feat: Added Create car, get cars, get car, update car, and delete car controller function.

--- src/api/v1/controllers/carControllers.ts
+++ src/api/v1/controllers/carControllers.ts
@@ -20,3 +20,72 @@ export const createCarHandler = async (
         next(error);
     }
 };
+
+// handles GET request to read all cars in collection
+export const getAllCarsHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const cars = await carService.getAllCars();
+
+        res.status(HTTP_STATUS.OK).json(successResponse({"count": cars.length, cars}, "Cars retrieved successfully"));
+    } catch (error: unknown) {
+        next(error);
+    }
+};
+
+// handles GET request to read a single car by ID
+export const getCarByIdHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const { id } = req.params;
+
+        const car = await carService.getCarById(id as string);
+
+        res.status(HTTP_STATUS.OK).json(successResponse({car}, "Car retrieved successfully"));
+    } catch (error: unknown) {
+        next(error);
+    }
+};
+
+// handles PUT request to update an existing car by ID
+export const updateCarHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const {model, brand, year, price, color, status} = req.body;
+        const updateData = {model, brand, year, price, color, status};
+
+        const updatedCar = await carService.updateCar(id as string, updateData);
+
+        res.status(HTTP_STATUS.OK).json(successResponse({updatedCar}, "Car updated successfully"));
+    } catch (error: unknown) {
+        next(error);
+    }
+};
+
+
+// handles DELETE request to delete an existing car
+export const deleteCarHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const { id } = req.params;
+        
+        await carService.deleteCar(id as string);
+
+        res.status(HTTP_STATUS.OK).json(successResponse({id}, "Car deleted successfully"));
+    } catch (error: unknown) {
+        next(error);
+    }
+};
