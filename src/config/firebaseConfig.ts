fde15506383f4a63b3d8648a304e63a5fb58c00d
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-04-15 16:54:22 -0500

    Feat: Configured firebase config to use authentification.

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,16 +1,31 @@
-import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
+import { ServiceAccount } from "firebase-admin/app";
+import admin from "firebase-admin";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
+import { getAuth, Auth } from "firebase-admin/auth";
 
-import * as serviceAccount from "../../drivetime-final-project.json";
+// update with your won firebase private key .json file path
+//import * as serviceAccount from "../comp3018-module4-firebase-adminsdk-fbsvc-34a8114031.json";
+
+import serviceAccountJson from "../../drivetime-final-project.json";
+
+const serviceAccount = serviceAccountJson as unknown as admin.ServiceAccount;
 
 // Initialize the Firebase app with the service account credentials
 // This step is necessary before you can use any Firebase services
+/*
 initializeApp({
     credential: cert(serviceAccount as ServiceAccount),
+});
+*/
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as ServiceAccount),
 });
 
 // Get a reference to the Firestore service
 // This creates a Firestore instance that you can use to interact with your database
 const db: Firestore = getFirestore();
 
-export { db };
+const auth: Auth = getAuth();
+
+export { db, auth };
